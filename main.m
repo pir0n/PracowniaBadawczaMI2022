0b1035870062e166c652ae61859de87a50ddd9f2
Kuba Gryglak <kuba.gryglak@student.put.poznan.pl>
2022-02-01 02:37:33 +0100

    First try to learn network

--- main.m
+++ main.m
@@ -1,6 +1,14 @@
 clear;
 
-ImgVector = zeros(2500,3457)
+ImgVector = zeros(2500,3457);
+ImgVector_class = [3457];
+for i = 1:119
+    ImgVector_class(i) = 0;
+end 
+for i = 120:3457
+    ImgVector_class(i) = 1;
+end 
+%load house_dataset;
 
 for k = 1:3457
 
@@ -9,3 +17,8 @@ for k = 1:3457
   Img = imread(imgFilename);
   ImgVector(:,k) = Img(:);
 end
+
+%ImgVector
+net = newfit(ImgVector,ImgVector_class,[3 5]);
+nftool
+%net=train(net,ImgVector,ImgVector_class);
