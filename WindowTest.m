e54aaa4643d5216e0ff89d653bfcd5018d7bebbd
KarolKob <42039707+KarolKob@users.noreply.github.com>
2022-02-09 01:35:31 +0100

    25x25 dataset

--- WindowTest.m
+++ WindowTest.m
@@ -2,8 +2,22 @@ imgFilename = 'KinFaceW-II/images/father-dau/fd_001_1.jpg';
 TestImg = imread(imgFilename);
 GrayImg = rgb2gray(TestImg);
 
-windowL = 10;
-for ind = 1:2:length(GrayImg(:,1))-windowL+1 % set the end such that you don't index more than the length of the array E.
-    Window = GrayImg(ind:ind+windowL-1, ind:ind+windowL-1,:); %pulled out section of E to be used 
-    WinArray = Window(:);
+y_max = -1000;
+
+windowL = 25;
+for ii = 1:5:length(GrayImg(:,1))-windowL+1 % set the end such that you don't index more than the length of the array E.
+    for jj = 1:5:length(GrayImg(1,:))-windowL+1
+        Window = GrayImg(ii:ii+windowL-1, jj:jj+windowL-1,:); %pulled out section of E to be used 
+        WinArray = Window(:);
+        %y_test = sim(net,double(WinArray)/255);
+
+        imshow(Window)
+        %pause
+    end
+    %if y_test > y_max
+     %   y_max = y_test;
+      %  y_win = Window;
+    %end
 end
+
+imshow(y_win)
--- main.m
+++ main.m
@@ -1,22 +1,28 @@
 clear;
 
-ImgVector = zeros(100,3457);
-ImgVector_class = [3457];
-for i = 1:206
-    ImgVector_class(i) = 0.0;
-end 
-for i = 207:3457
+ImgVector = zeros(625,3544);
+ImgVector_class = [3544];
+
+for i = 1:3338
     ImgVector_class(i) = 1.0;
 end 
+
+for i = 3338:3544
+    ImgVector_class(i) = 0.0;
+end 
+
 %load house_dataset;
 
-for k = 1:3457
+for k = 1:3544
 
   number_str = num2str(k,'%04.f');
-  imgFilename = sprintf('dataset/image%s.png', number_str);
+  imgFilename = sprintf('new_dataset/image%s.png', number_str);
   Img = imread(imgFilename);
   ImgVector(:,k) = Img(:);
 end
+
+%imshow(Img)
+%pause
 
 %ImgVector
 %net = newfit(ImgVector,ImgVector_class,[50 50]);
@@ -27,9 +33,9 @@ end
 %net = network(50,2);
 net = newff(ImgVector,ImgVector_class,[25 25]);
 %nftool
-net.trainParam.goal = 10^(-700);
-net.trainParam.min_grad = 0.00000000001;
-net.trainParam.max_fail= 100;
+%net.trainParam.goal = 10^(-700);
+%net.trainParam.min_grad = 0.00000000001;
+%net.trainParam.max_fail= 100;
 %net.trainParam.goal = 0.000001;
 net = train(net,ImgVector,ImgVector_class);
 y = sim(net,ImgVector);
@@ -40,14 +46,21 @@ imgFilename = 'KinFaceW-II/images/father-dau/fd_001_1.jpg';
 TestImg = imread(imgFilename);
 GrayImg = rgb2gray(TestImg);
 
-windowL = 10;
-for ind = 1:2:length(GrayImg(:,1))-windowL+1 % set the end such that you don't index more than the length of the array E.
-    Window = GrayImg(ind:ind+windowL-1, ind:ind+windowL-1,:); %pulled out section of E to be used 
-    WinArray = Window(:);
-    y_test = sim(net,WinArray);
-    
-    if y_test > 0.5
-        y_win = Window;
+y_max = -1000;
+
+windowL = 25;
+for ii = 1:5:length(GrayImg(:,1))-windowL+1 % set the end such that you don't index more than the length of the array E.
+    for jj = 1:5:length(GrayImg(1,:))-windowL+1
+        Window = GrayImg(ii:ii+windowL-1, jj:jj+windowL-1,:); %pulled out section of E to be used 
+        WinArray = Window(:);
+        y_test = sim(net,double(WinArray)/255);
+
+        %imshow(Window)
+        %pause
+        if y_test > y_max
+            y_max = y_test;
+            y_win = Window;
+        end
     end
 end
 
